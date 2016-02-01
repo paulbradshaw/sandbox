fcd2646c0be2209a1efc8d2a31356a08c49f9a50
Paul Bradshaw <paulonhismobile@gmail.com>
2016-02-01 17:11:56 +0000

    Change to mechanize scraper

--- scraper.py
+++ scraper.py
@@ -1,48 +1,25 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
-import scraperwiki
+###############################################################################
+# START HERE: Tutorial for scraping pages behind form, using the
+# very powerful Mechanize library. Documentation is here: 
+# http://wwwsearch.sourceforge.net/mechanize/
+###############################################################################
+import mechanize 
 import lxml.html
-import mechanize
 
-print 'hello'
-urltoscrape = 'http://site.com/'
-print urltoscrape
-urltoscrape2 = urltoscrape+'/p1'
-print urltoscrape2
-listylist = ['p1','p2','p3']
-for blah in listylist:
-    print blah
-    fullurl = urltoscrape+blah
-    print fullurl
-#
-# # Read in a page
-html = scraperwiki.scrape("http://uk.soccerway.com/teams/netherlands/fortuna-sittard/1551/")
-#
-# # Find something on the page using css selectors
-root = lxml.html.fromstring(html)
-tds = root.cssselect("table.table.squad.sortable td div")
-#print tds
+lotterygrantsurl = "http://www.lottery.culture.gov.uk/AdvancedSearch.aspx"
 
-indexno = 0
-record = {}
-for td in tds:
-    indexno = indexno+1
-    #print lxml.html.tostring(td)
-    #print td.text
-    fullentry = lxml.html.tostring(td)
-    record['td'] = fullentry
-    record['index'] = indexno
-    scraperwiki.sqlite.save(unique_keys=['index'], data=record)
-#
-# # Write out to the sqlite database using scraperwiki library
+br = mechanize.Browser()
+response = br.open(lotterygrantsurl)
 
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+print "All forms:", [ form.name  for form in br.forms() ]
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+br.select_form(name="aspnetForm")
+print br.form
+
+br["ctl00$phMainContent$dropDownAwardDate"] = ["Between"]
+br["ctl00$phMainContent$txtGrantDateFrom"] = "01/01/2004"
+br["ctl00$phMainContent$txtGrantDateTo"]  = "20/01/2004"
+
+
+response = br.submit()
+print response.read()
